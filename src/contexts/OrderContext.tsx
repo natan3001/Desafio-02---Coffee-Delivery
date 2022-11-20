50e33246177731ff0a59bfab9a157884de4354c1
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-20 18:45:29 -0300

    Criado o context provider do pedido

--- src/contexts/OrderContext.tsx
+++ src/contexts/OrderContext.tsx
@@ -1,9 +1,36 @@
-import { ReactNode } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
+// Order
+export interface OrderProps {
+  zipCode: string
+  address: string
+  address2: string
+  number: string
+  district: string
+  city: string
+  state: string
+  paymentMethod: string
+
+  totalOfItems: number
+  deliveryFee: number
+  total: number
+}
+
+// Context
 interface OrderContextProviderProps {
   children: ReactNode
 }
 
+interface OrderContextType {
+  order: OrderProps
+}
+
+const OrderContext = createContext({} as OrderContextType)
+
 export function OrderContextProvider({ children }: OrderContextProviderProps) {
-  return <>{children}</>
+  const [order, setOrder] = useState({} as OrderProps)
+
+  return (
+    <OrderContext.Provider value={{ order }}>{children}</OrderContext.Provider>
+  )
 }
