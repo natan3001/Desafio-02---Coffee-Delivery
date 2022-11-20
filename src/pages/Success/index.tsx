f22da499a4486d2f2b57400610a4043267baae04
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-20 15:46:49 -0300

    Criado e estilizado a pagina de pedido feito com sucesso!

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,50 @@
+import { Clock, CurrencyDollar, MapPin } from "phosphor-react";
+import { OrderDetails, OrderDetailsItem, SuccessContainer } from "./styles";
+
+import CoffeeDeliverySuccess from './../../assets/coffee-delivery-success.png'
+
 export function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <div>
+        <h1>Uhu! Pedido confirmado</h1>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+      </div>
+
+      <OrderDetails>
+        <ul>
+          <OrderDetailsItem color="purple">
+            <span>
+              <MapPin size={16} weight="fill" />
+            </span>
+            <div>
+              <span>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+              </span>
+              <span>Farrapos - Porto Alegre, RS</span>
+            </div>
+          </OrderDetailsItem>
+          <OrderDetailsItem color="yellow">
+            <span>
+              <Clock size={16} weight="fill" />
+            </span>
+            <div>
+              <span>Previsão de entrega</span>
+              <span><strong>20 min - 30 min </strong></span>
+            </div>
+          </OrderDetailsItem>
+          <OrderDetailsItem color="yellow-dark">
+            <span>
+              <CurrencyDollar size={16} weight="fill" />
+            </span>
+            <div>
+              <span>Pagamento na entrega</span>
+              <span><strong>Cartão de Crédito</strong></span>
+            </div>
+          </OrderDetailsItem>
+        </ul>
+        <img src={CoffeeDeliverySuccess} alt="" />
+      </OrderDetails>
+    </SuccessContainer>
   )
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+
+export const SuccessContainer = styled.div`
+  margin-top: 5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+
+  h1{
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 2rem;
+    line-height: 1.3;
+
+    color: ${props => props.theme["yelllow-700"]};
+  }
+
+  p{
+    font-size: 1.25rem;
+    color: ${props => props.theme["base-500"]};
+  }
+`
+
+const ICON_COLOR = {
+  'purple': 'purple-500',
+  'yellow': 'yelllow-500',
+  'yellow-dark': 'yelllow-700',
+} as const
+
+interface OrderDetailsIconProps {
+  color: keyof typeof ICON_COLOR
+}
+
+export const OrderDetailsItem = styled.li<OrderDetailsIconProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  > span{
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${props => props.theme[ICON_COLOR[props.color]]};
+  }
+
+  div{
+    display: flex;
+    flex-direction: column;
+    gap: .25rem;
+    
+    color: ${props => props.theme["base-400"]};
+
+    span{
+      width: 100%;
+    }
+  }
+  
+  svg{
+    color: ${props => props.theme["base-50"]};
+  }
+`
+
+export const OrderDetails = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 6.375rem;
+
+  align-items: center;
+
+  ul{
+    position: relative;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 2.25rem;
+    padding: 2.5rem;
+    border: 1px solid transparent;
+  }
+
+  ul:before {
+    content: "";
+    position: absolute;
+    inset: 0;
+    border-radius: 6px 36px 6px 36px; 
+    padding: 2px; 
+    background: linear-gradient(135deg, ${props => props.theme["yelllow-500"]}, ${props => props.theme["purple-500"]});
+
+    -webkit-mask: 
+      linear-gradient(#fff 0 0) content-box, 
+      linear-gradient(#fff 0 0);
+    -webkit-mask-composite: xor;
+      mask-composite: exclude; 
+  }
+`
